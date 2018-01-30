975f60267fdfd366beaac11120bd2ff9ec2dfa0b
Matt Layton <layton.matt1986@gmail.com>
2018-01-29 21:34:30 -0600

    Finish adding comment POST route

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var app 				= require('express')(),
 		bodyParser 	= require('body-parser'),
 
 		Campground 	= require('./models/campground'),
+		Comment 		= require('./models/comment'),
 
 		seedDB 			= require('./seeds');
 
@@ -90,7 +91,39 @@ app.get('/campgrounds/:id/comments/new', (req, res) => {
 			res.render('comments/new', { campground: campground });
 		}
 	});
-	
+});
+
+app.post('/campgrounds/:id/comments', (req, res) => {
+	// look up campground using ID
+	Campground.findById(req.params.id, (err, campground) => {
+		if (err) {
+			console.log(err);
+			res.redirect('/campgrounds');
+		} else {
+			// create new comment
+			Comment.create(req.body.comment, (err, comment) => {
+				if (err) {
+					console.log(err);
+				} else {
+					// connect new comment to campground
+					campground.comments.push(comment._id);
+					campground.save();
+					// redirect to campground show page
+
+					console.log(campground._id, 
+											typeof campground._id,
+											'/campgrounds/' + campground._id);
+					console.log(req.params.id, 
+											typeof req.params.id,
+											'/campgrounds/' + req.params.id);
+
+
+
+					res.redirect('/campgrounds/' + req.params.id);
+				}
+			});	
+		}
+	});
 });
 
 
@@ -99,3 +132,4 @@ app.get('/campgrounds/:id/comments/new', (req, res) => {
 app.listen(3000, () => {
 	console.log('YelpCamp server is running on port 3000');
 });
+
