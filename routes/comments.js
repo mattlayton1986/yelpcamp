a1b0ec5983076cb5bd592f3d1560a9e776768aef
Matt Layton <layton.matt1986@gmail.com>
2018-02-03 18:43:31 -0600

    Add destroy route for comments

--- routes/comments.js
+++ routes/comments.js
@@ -67,6 +67,17 @@ router.put('/:comment_id', (req, res) => {
 	});
 });
 
+// DESTROY
+router.delete('/:comment_id', (req, res) => {
+	Comment.findByIdAndRemove(req.params.comment_id, (err) => {
+		if (err) {
+			res.redirect('back');
+		} else {
+			res.redirect('/campgrounds/' + req.params.id);
+		}
+	});
+});
+
 // Middleware
 function isLoggedIn(req, res, next) {
 	if (req.isAuthenticated()) {
